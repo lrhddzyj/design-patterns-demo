9f920dcf52507fb5df24fd7e3588e1272f5c694c
lrh <lirh@gmail.com>
2020-05-15 15:02:23 +0800

    observer pattern

--- src/main/java/com/lrh/observer/Client.java
+++ src/main/java/com/lrh/observer/Client.java
@@ -3,20 +3,24 @@ package com.lrh.observer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Observable;
 
 /**
  * @description:
  * @author: lrh
  * @date: 2020/5/15 14:23
  */
-public class Client {
+public class Client extends Observable {
 
 	public static void main(String[] args) throws IOException {
+		Client client = new Client();
 		AbstractDnsServer sh = new SHDnsServer();
 		AbstractDnsServer cn = new ChinaDnsServer();
 		AbstractDnsServer top = new TopDnsServer();
 		cn.setUpperServer(top);
 		sh.setUpperServer(cn);
+		client.addObserver(sh);
+
 		System.out.printf("====域名解析模拟器====");
 		while (true) {
 			System.out.println("\n请输入域名（输入N退出）");
@@ -26,10 +30,16 @@ public class Client {
 			}
 			Recorder recorder = new Recorder();
 			recorder.setDomain(domain);
-			sh.update(null, recorder);
+			client.setChanged();
+			client.notifyObservers(recorder);
+//			sh.update(null, recorder);
 			System.out.println("-----DNS服务器解析结果------");
 			System.out.println(recorder);
 		}
 
 	}
+
+
+
+
 }
